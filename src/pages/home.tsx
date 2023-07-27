5926cb2e6c0adc44fd7930454bba6b016dab0e28
Segni Adeba <61460186+SegniAdebaGodsSon@users.noreply.github.com>
2023-07-27 17:15:54 +0300

    removed non-working code

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,27 +2,6 @@ import { api } from "~/utils/api";
 
 export default function UserHome() {
 
-    const { data: consultantRoleProjects, isLoading: consultantRoleProjectsIsLoading, error: consultantRoleProjectsError } = api.project.getAllWhereRoleIsConsultant.useQuery({
-        page: 1,
-        limit: 5,
-        orderBy: 'latest',
-        status: 'ACCEPTED'
-    });
-
-    const { data: contractorRoleProjects, isLoading: contractorRoleProjectsIsLoading, error: contractorRoleProjectsError } = api.project.getAllWhereRoleIsContractor.useQuery({
-        page: 1,
-        limit: 5,
-        orderBy: 'latest',
-        status: 'ACCEPTED'
-    });
-
-    const { data: siteManagerRoleProjects, isLoading: siteManagerRoleProjectsIsLoading, error: siteManagerRoleProjectsError } = api.project.getAllWhereRoleIsSiteManager.useQuery({
-        page: 1,
-        limit: 5,
-        orderBy: 'latest',
-        status: 'ACCEPTED'
-    });
-
     return (
         <main>
             <section>
